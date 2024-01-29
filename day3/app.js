e1c43d8a7572705d2c3a2f37182803c9b1b7f7b3
Piyush <piyushmore109@gmail.com>
2024-01-29 15:26:00 +0530

    day3-4

--- day3/app.js
+++ day3/app.js
@@ -1,8 +1,21 @@
-import  React from "react"
-import  ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id:"heading"}, "Hello World!");
+// const heading = React.createElement("h1", { id: "heading" }, "React basics");
+
+const Title = () => (
+  <h1 id="heading" className="Heading">
+    Hello World
+  </h1>
+);
+
+const HedingComponent = () => {
+  return <div>
+    <Title />
+    <h1>Hello from functional component</h1>
+  </div>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); 
+root.render(<HedingComponent />);
