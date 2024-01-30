7b78fec2bcc9cb9ee7ae0121b52668284379c7cb
Piyush <piyushmore109@gmail.com>
2024-01-30 20:47:29 +0530

    restaurant card

--- day3/app.js
+++ day3/app.js
@@ -2,51 +2,59 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // const heading = React.createElement("h1", { id: "heading" }, "React basics");
-const Header = ()=>{
-  return(
+const Header = () => {
+  return (
     <div className="header">
       <div className="logoContainer">
-        <img className="logo" src="https://img.freepik.com/free-vector/lovely-pizza-delivery-man-scooter_23-2147681935.jpg?w=740&t=st=1706608416~exp=1706609016~hmac=7d1b5d61ac2721ee6117b052e1258e7287213387d13f4dd9f6bb5559b6afb0cb" />
+        <img
+          className="logo"
+          src="https://img.freepik.com/free-vector/lovely-pizza-delivery-man-scooter_23-2147681935.jpg?w=740&t=st=1706608416~exp=1706609016~hmac=7d1b5d61ac2721ee6117b052e1258e7287213387d13f4dd9f6bb5559b6afb0cb"
+        />
       </div>
       <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-const Restaurantcard = ()=>{
+const Restaurantcard = () => {
   return (
     <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/b2jeywla2n5b8h25ey1p"
+      />
       <h3>Meghana Foods</h3>
     </div>
-  )
-}
+  );
+};
 
-const Body = ()=>{
-  return(
+const Body = () => {
+  return (
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
         <Restaurantcard />
       </div>
     </div>
-  )
-}
+  );
+};
 
-const AppLayout = () =>{
+const AppLayout = () => {
   return (
     <div className="App">
       <Header />
       <Body />
     </div>
-  )
-}
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
