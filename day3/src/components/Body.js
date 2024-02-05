0898aadd0c00a457c3931d34899f9234251e87f6
Piyush <piyushmore109@gmail.com>
2024-02-05 09:34:16 +0530

    shimmer UI

--- day3/src/components/Body.js
+++ day3/src/components/Body.js
@@ -1,30 +1,41 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurants from "../utils/mock_data";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+
 const Body = () => {
-  const [resList,setResList] = useState([]);
+  const [resList, setResList] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[])
+  }, []);
 
-  const fetchData = async ()=>{
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
-    const json = await data.json()
+    const json = await data.json();
 
     console.log(json);
-    setResList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-  }
+    setResList(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
+  // if(resList.length===0){
+  //   return <Shimmer />
+  // }
 
-  return (
+  return resList.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() =>
-             setResList(resList.filter(res => res.info.avgRating>="4.5"))
+            setResList(resList.filter((res) => res.info.avgRating >= "4.5"))
           }
         >
           Top Rated Restaurants
